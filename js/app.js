8105aab84f50da087e772d8eb636f1c1e98d2f10
alemela <alessiom92@gmail.com>
2015-08-27 16:13:39 +0200

    added filter (just a little demo)

--- js/app.js
+++ js/app.js
@@ -1,70 +1,90 @@
 var basemap = new L.TileLayer(baseUrl, {maxZoom: 17, attribution: baseAttribution, subdomains: subdomains, opacity: opacity});
 
-var center = new L.LatLng(0, 0);
+/*var menuButton = L.control({ position: 'bottomright' });
 
-var map = new L.Map('map', {center: center, zoom: 2, maxZoom: maxZoom, layers: [basemap]});
+menuButton.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'mini-submenu');
+    this._div.innerHTML = '';
+    return this._div;
+};
+menuButton.addTo(map);*/
 
 var popupOpts = {
     autoPanPadding: new L.Point(5, 50),
     autoPan: true
 };
 
-var points = L.geoCsv (null, {
+var railwaystation = L.geoCsv (null, {
     firstLineTitles: true,
     fieldSeparator: fieldSeparator,
     onEachFeature: function (feature, layer) {
-        var popup = '<div class="popup-content"><table class="table table-striped table-bordered table-condensed">';
-        for (var clave in feature.properties) {
-            var title = points.getPropertyTitle(clave).strip();
-            var attr = feature.properties[clave];
-            if (title == labelColumn) {
-                layer.bindLabel(feature.properties[clave], {className: 'map-label'});
-            }
-            if (title === "title") {
-                attr = '<a target="_blank" href="//it.wikipedia.org/wiki/' + attr + '">'+ attr + '</a>';
-            }
-            if (attr) {
-                popup += '<tr><th>'+title+'</th><td>'+ attr +'</td></tr>';
-            }
-        }
-        popup += "</table></popup-content>";
-        layer.bindPopup(popup, popupOpts);
+        popupGenerator(feature, layer);
     },
     pointToLayer: function (feature, latlng) {
-        for (var clave in feature.properties) {
-            var title = points.getPropertyTitle(clave).strip();
-            var attr = feature.properties[clave];
-            if (title === "type") {
-                if (attr === "airport") {
-                    return L.marker(latlng, { icon: airportMarker });
-                }
-                if (attr === "adm1st" || attr === "adm2nd" || attr === "adm3rd" || attr === "city") {
-                    return L.marker(latlng, { icon: cityMarker });
-                }
-                if (attr === "railwaystation") {
-                    return L.marker(latlng, { icon: stationMarker });
-                }
-            }
-        }
-        return L.marker(latlng, { icon: blueMarker });
+        return L.marker(latlng, { icon: stationMarker });
+    },
+    filter: function (feature, layer) {
+        return feature.properties.type === "railwaystation";
     }
 });
 
-var hits = 0;
-var total = 0;
+var airport = L.geoCsv (null, {
+    firstLineTitles: true,
+    fieldSeparator: fieldSeparator,
+    onEachFeature: function (feature, layer) {
+        popupGenerator(feature, layer);
+    },
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, { icon: airportMarker });
+    },
+    filter: function (feature, layer) {
+        return feature.properties.type === "airport";
+    }
+});
+
+var city = L.geoCsv (null, {
+    firstLineTitles: true,
+    fieldSeparator: fieldSeparator,
+    onEachFeature: function (feature, layer) {
+        popupGenerator(feature, layer);
+    },
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, { icon: cityMarker });
+    },
+    filter: function (feature, layer) {
+        return feature.properties.type === "city" || feature.properties.type === "am3rd" || feature.properties.type === "am2nd" || feature.properties.type === "am1st";
+    }
+});
+
+var map = new L.Map('map', {
+    center: new L.LatLng(0, 0),
+    zoom: 2, 
+    maxZoom: maxZoom, 
+    layers: [basemap]
+});
+
 var markers = new L.MarkerClusterGroup();
 var dataCsv;
 
 var addCsvMarkers = function() {
-    hits = 0;
-    total = 0;
 
     map.removeLayer(markers);
-    points.clearLayers();
+    railwaystation.clearLayers();
+    airport.clearLayers();
 
     markers = new L.MarkerClusterGroup(clusterOptions);
-    points.addData(dataCsv);
-    markers.addLayer(points);
+
+    //creo layers
+    airport.addData(dataCsv);
+    railwaystation.addData(dataCsv);
+    railwaystation.addData(dataCsv); //XXX doppia chiamata per farlo funzionare
+    city.addData(dataCsv);
+    city.addData(dataCsv);
+
+    //aggrego layers in markers
+    markers.addLayer(airport);
+    markers.addLayer(railwaystation);
+    markers.addLayer(city);
 
     map.addLayer(markers);
     try {
@@ -78,23 +98,26 @@ var addCsvMarkers = function() {
     return false;
 };
 
-function ArrayToSet(a) {
-    var temp = {};
-    for (var i = 0; i < a.length; i++)
-        temp[a[i]] = true;
-    var r = [];
-    for (var k in temp)
-        r.push(k);
-    return r;
+var popupGenerator = function(feature, layer) {
+    var popup = '<div class="popup-content"><table class="table table-striped table-bordered table-condensed">';
+        for (var clave in feature.properties) {
+            var title = airport.getPropertyTitle(clave).strip(); //XXX airport isn't generic
+            var attr = feature.properties[clave];
+            if (title == labelColumn) {
+                layer.bindLabel(feature.properties[clave], {className: 'map-label'});
+            }
+            if (title === "title") {
+                attr = '<a target="_blank" href="//it.wikipedia.org/wiki/' + attr + '">'+ attr + '</a>';
+            }
+            if (attr) {
+                popup += '<tr><th>'+title+'</th><td>'+ attr +'</td></tr>';
+            }
+        }
+        popup += "</table></popup-content>";
+        layer.bindPopup(popup, popupOpts);
 }
 
-if(typeof(String.prototype.strip) === "undefined") {
-    String.prototype.strip = function() {
-        return String(this).replace(/^\s+|\s+$/g, '');
-    };
-}
-
-map.addLayer(markers);
+/* CSV LOADING */
 
 $(document).ready( function() {
     $.ajax ({
@@ -110,10 +133,41 @@ $(document).ready( function() {
             addCsvMarkers();
         }
     });
-
-    $("#clear").click(function(evt){
-        evt.preventDefault();
-        addCsvMarkers();
-    });
-
 });
+
+/* LAYERS CONTROL */ //XXX too much hardwritten
+
+var overlayMaps = {};
+var emptyLayers = {
+    "Aeroporti":  new L.layerGroup().addTo(map),
+    "Stazioni":  new L.layerGroup().addTo(map)
+};
+for (var index in emptyLayers) {
+    overlayMaps[index] = emptyLayers[index];
+}
+
+var control = L.control.layers(null, overlayMaps);
+control.addTo(map);
+
+map.on('overlayadd', function (a) {
+    if (a.name === "Aeroporti") {
+        markers.addLayer(airport);
+    } else if (a.name === "Stazioni") {
+        markers.addLayer(railwaystation);
+    }
+});
+map.on('overlayremove', function (a) {
+    if (a.name === "Aeroporti") {
+        markers.removeLayer(airport);
+    } else if (a.name === "Stazioni") {
+        markers.removeLayer(railwaystation);
+    }
+});
+
+/* UTILS */
+
+if(typeof(String.prototype.strip) === "undefined") {
+    String.prototype.strip = function() {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
