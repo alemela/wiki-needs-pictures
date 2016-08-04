2a1a52cfc7aff9c5ee5967033a121c9ea75f5587
alemela <alessiom92@gmail.com>
2016-08-04 02:47:19 +0200

    changed attributions

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var dataUrl = 'data/data.csv';
 var maxZoom = 16;
 var fieldSeparator = ',';
 var baseUrl = '//maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
-var baseAttribution = 'Data, imagery and map information provided by <a href="http://open.mapquest.co.uk" target="_blank">MapQuest</a>, <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
+var baseAttribution = 'Kudos to <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> and contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 var subdomains = '1234';
 var clusterOptions = {showCoverageOnHover: false, maxClusterRadius: 50};
 var labelColumn = "title";
