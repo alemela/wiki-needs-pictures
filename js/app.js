71b506172a477990a447ef93fdc259e1f1cb9bea
alemela <alessiom92@gmail.com>
2016-08-04 16:24:17 +0200

    moved messages outside app.js

--- js/app.js
+++ js/app.js
@@ -192,10 +192,7 @@ map.on('overlayremove', function (a) {
 L.easyButton('fa-info', function() {
     bootbox.dialog({
         title: "<center>Wiki Needs Pictures</center>",
-        message: '<p><b>Many Wikipedia articles have no images</b> or only few. Some of them talking about <b>things that are out there</b>, ready to be photographed. <b>Find them all!</b></p><hr/>' +
-                 '<p>Each suggestion has a link to it\'s Wikipedia or Wikidata page and to the reason why is present here. If you want to make a pin disappear you have to resolve it\'s problem (e.g. removing the warning template in it\'s page or adding the image to Wikidata).</p>' +
-                 '<p>For any trouble or suggestion write on <a href="https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures" target="_blank">this page</a>. This is an open source project! Let\'s code together in this <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">Github repository.</a></p>' +
-                 '<p><small>The tool updates the view once a day. Please be patient, it takes a while to load all the data.<small></p>'
+        message: messageInABottle
     });
 }, "Info", { position: 'bottomleft' }).addTo(map);
 
---
+++ js/message.js
@@ -0,0 +1,4 @@
+var messageInABottle = '<p><b>Many Wikipedia articles have no images</b> or only few. Some of them talking about <b>things that are out there</b>, ready to be photographed. <b>Find them all!</b></p><hr/>' +
+         '<p>Each suggestion has a link to it\'s Wikipedia or Wikidata page and to the reason why is present here. If you want to make a pin disappear you have to resolve it\'s problem (e.g. removing the warning template in it\'s page or adding the image to Wikidata).</p>' +
+         '<p>For any trouble or suggestion write on <a href="https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures" target="_blank">this page</a>. This is an open source project! Let\'s code together in this <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">Github repository.</a></p>' +
+         '<p><small>The tool updates the view once a day. Please be patient, it takes a while to load all the data.<small></p>'
