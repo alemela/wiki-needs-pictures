5684dc9795351ba3828e4324c6285d5f83e0a859
alemela <alessiom92@gmail.com>
2016-08-05 17:32:34 +0200

    added Spanish

--- js/message.js
+++ js/message.js
@@ -1,3 +1,5 @@
+var talk_page = "https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures"
+
 var message_EN =
   '<p><b>Many Wikipedia articles have no images</b> or only few. Some of them talk about <b>things that are out there</b>, ready to be photographed. <b>Find them all!</b></p><hr/>' +
   '<p><b>Instructions</b></p>' +
@@ -7,7 +9,7 @@ var message_EN =
   '<li>Pass over a pinpoint to see its name. Make a click for more details and links.</li></ul>' +
   'Take pictures and upload them to Wikimedia Commons, then resolve the related warning (e.g. removing the template in its page or adding the image to Wikidata) and on the next run you will see the pinpoint disappear. Great job!' +
   '</p><hr/>' +
-  '<p>For any trouble or suggestion write on <a href="https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures" target="_blank">this page</a>. This is an open source project! Let\'s code together in this <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">Github repository.</a></p>' +
+  '<p>For any trouble or suggestion write on <a href="' + talk_page + '" target="_blank">this page</a>. This is an open source project! Let\'s code together in this <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">Github repository.</a></p>' +
   '<p><small>The tool updates the view once a day. Please be patient, it takes a while to load all the data.<small></p>'
 
 var message_IT =
@@ -19,7 +21,7 @@ var message_IT =
   '<li>Passa sopra un segnaposto per vederne il nome. Cliccaci sopra per avere maggiori dettagli e collegamenti.</li></ul>' +
   'Scatta fotografie e caricale su Wikimedia Commons, poi risolvi il relativo problema (ad esempio rimuovendo un template nella pagina di discussione o aggiungendo l\'immagine su Wikidata) e al prossimo aggiornamento vedrai il segnaposto scomparire. Ottimo lavoro!' +
   '</p><hr/>' +
-  '<p>Per qualunque problema o suggerimento scrivi su <a href="https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures" target="_blank">questa pagina</a>. Questo è un progetto open source! Sviluppiamolo insieme su questo <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">repository Github.</a></p>' +
+  '<p>Per qualunque problema o suggerimento scrivi su <a href="' + talk_page + '" target="_blank">questa pagina</a>. Questo è un progetto open source! Sviluppiamolo insieme su questo <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">repository Github.</a></p>' +
   '<p><small>Il programma aggiorna i dati una volta al giorno. Sii paziente, occorre un po\' di tempo per caricare tutti i dati.<small></p>'
 
 var message_DE =
@@ -31,7 +33,7 @@ var message_DE =
   '<li>Berühren Sie ein Objekt, um dessen Namen zu sehen. Klicken Sie darauf, um weitere Informationen und Links zu erhalten.</li></ul>' +
   'Machen Sie Fotos und laden Sie sie auf Wikimedia Commons hoch, dann beheben Sie die damit verbundene Warnung (z.B. löschen Sie die Vorlage in der Seite oder fügen Sie auf Wikidata das Bild hinzu).' +
   '</p><hr/>' +
-  '<p>Für Probleme oder Anregungen schreiben Sie auf <a href="https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures" target="_blank">diese Seite</a>. Dies ist ein open-Source-Projekt! Programmieren wir zusammen in diesem <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">Github Repository!</a></p>' +
+  '<p>Für Probleme oder Anregungen schreiben Sie auf <a href="' + talk_page + '" target="_blank">diese Seite</a>. Dies ist ein open-Source-Projekt! Programmieren wir zusammen in diesem <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">Github Repository!</a></p>' +
   '<p><small>Das Tool aktualisiert die Ansicht täglich. Bitte seien Sie geduldig: es dauert eine Weile, bis die Daten geladen sind.<small></p>'
 
 var message_FR =
@@ -43,5 +45,17 @@ var message_FR =
   '<li>Passez sur un "épingle" pour voir son nom. Faites un clic pour plus de détails et de liens.</li></ul>' +
   'Prenez des photos et téléchargez-les sur Wikimedia Commons, puis résoudrez l\'alerte (par exemple enlevez le modèle dans sa page ou ajoutez l\'image à Wikidata) et lors de la prochaine exécution vous verrez l\'épingle disparaître. Super boulot !' +
   '</p><hr/>' +
-  '<p>Pour tout problème ou suggestion écrivez sur <a href="https://meta.wikimedia.org/wiki/Grants_talk:IEG/Wiki_needs_pictures" target="_blank">cette page</a>. Il s\'agit d\'un projet open source ! Nous allons coder ensemble dans ce <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">référentiel Github.</a></p>' +
+  '<p>Pour tout problème ou suggestion écrivez sur <a href="' + talk_page + '" target="_blank">cette page</a>. Il s\'agit d\'un projet open source ! Nous allons coder ensemble dans ce <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">référentiel Github.</a></p>' +
   '<p><small>L\'outil met à jour l\'Interface une fois par jour. S\'il vous plaît soyez patient, il faut un certain temps pour charger toutes les données.<small></p>'
+
+var message_ES =
+  '<p><b>Muchos artículos de Wikipedia tienen pocas imágenes o ninguna</b>. Algunos de ellos hablan de <b>cosas que están ahí</b>, listas para ser fotografiadas. <b>¡Encontradlos todos!</b></p><hr/>' +
+  '<p><b>Instrucciones</b></p>' +
+  '<p><ul><li>Utilice el botón de arriba a la izquierda o haga doble clic en el mapa para ajustar el zoom. También puede hacer clic en un clúster de cerca de la ubicación que desee explorar.</li>' +
+  '<li>Utilice el botón superior derecho para filtrar lugares indicando la categoría.</li>' +
+  '<li>Utilice el botón de abajo a la derecha para ir a su posición actual.</li>' +
+  '<li>Pase por un lugar para ver su nombre. Haga un clic para ver más detalles y enlaces.</li></ul>' +
+  'Haga fotos y súbalas a Wikimedia Commons, después haga caso de la advertencia relacionada (por ejemplo quite la plantilla en su página o añada la imagen a Wikidata) y la próxima vez verá desaparecer los lugares/las imágenes. ¡Buen trabajo!' +
+  '</p><hr/>' +
+  '<p>Para cualquier problema o sugerencia, escriba a <a href="' + talk_page + '" target="_blank">esta página</a>. Este es un proyecto en código abierto ¡Codificamos juntos en este <a href="https://github.com/alemela/wiki-needs-pictures" target="_blank">repositorio de Github</a>!.</p>' +
+  '<p><small>La herramienta actualiza la vista una vez al día. Por favor tenga paciencia, tarda un poco en cargar todos los datos.</small></p>'
