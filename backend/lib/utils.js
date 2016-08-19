b6c5a918c486cb7facc860b59f2dfdbff1b111f2
alemela <alessiom92@gmail.com>
2016-08-19 16:58:30 +0200

    patches for the workflow

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var visitedCat = [];
 
-var digSimpleCategory = function (client, cat, code) { 
+var digSimpleCategory = function (client, cat, code) {
     client.getPagesInCategory(cat, function(err, pages) {
         pages.forEach(function(page) {
             locatePage(client, page.title, code);
@@ -18,7 +18,7 @@ var digCategoryTalk = function (client, cLabel, cat, tLabel, code) {
             return;
     }
     visitedCat.push(cat);
-    
+
     client.getPagesInCategory(cat, function(err, pages) {
         pages.forEach(function(page) {
             if (page.title.indexOf(cLabel) > -1) {
@@ -38,7 +38,7 @@ var digCategory = function (client, cLabel, cat, code) {
             return;
     }
     visitedCat.push(cat);
-    
+
     client.getPagesInCategory(cat, function(err, pages) {
         pages.forEach(function(page) {
             if (page.title.indexOf(cLabel) > -1) {
@@ -62,7 +62,7 @@ var locatePage = function (client, pagename, code) {
     client.api.call(params, function(err, info, next, data) {
         for (id in info.pages) {
             if (info.pages[id].coordinates !== undefined) {
-                var raw = info.pages[id].title+','+info.pages[id].coordinates[0].lat+','+info.pages[id].coordinates[0].lon+','+info.pages[id].coordinates[0].type+','+code+'\n';
+                var raw = '"'+info.pages[id].title+'",'+info.pages[id].coordinates[0].lat+','+info.pages[id].coordinates[0].lon+','+info.pages[id].coordinates[0].type+','+code+'\n';
                 fs.appendFile("temp.csv", raw, function(err) {
                 });
             }
@@ -74,4 +74,3 @@ exports.digSimpleCategory = digSimpleCategory;
 exports.digCategory = digCategory;
 exports.digCategoryTalk = digCategoryTalk;
 exports.locatePage = locatePage;
-
--- backend/update.sh
+++ backend/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for file in projects/*.js
-do
-    nodejs $file
-done
+#for file in projects/*.js
+#do
+#    nodejs $file
+#done
 
 #remove duplicated lines
 sort -u temp.csv > sorted.csv
@@ -15,12 +15,15 @@ sed -i '0,/^/s//title,lat,lng,type,code\n/' sorted.csv
 #save current dataset into backup directory
 mkdir -p ../data/backup
 date=`date +"%Y-%m-%d_%H-%M-%S"`
-mv ../data/data.csv ../data/backup/$date.csv 2>/dev/null
+mkdir ../data/backup/$date
+mv ../data/data.csv ../data/backup/$date 2>/dev/null
 
 #make current the newly generated one
 mv sorted.csv ../data/data.csv
 
 #split by countries
+mv ../data/countries ../data/backup/$date 2>/dev/null
+mkdir ../data/countries
 python geo.py
 
 #add header
